445d7f0bcd41d961a425ecca602db4cbcdc08ee7
David <euntek95@gmail.com>
2024-03-10 20:50:39 -0700

    add game over screen

--- src/index2.js
+++ src/index2.js
@@ -1,5 +1,10 @@
 const c = canvas.getContext('2d')
-const img = document.querySelector('#myImage')
+// const playerShoot = document.querySelector('#playerShoot')
+const gameOverScreen = document.querySelector('#gameOver')
+const scoreEle = document.querySelector('#scoreEle')
+
+c.imageSmoothingEnabled = true
+c.imageSmoothingQuality = 'high'
 
 class Player {
     constructor(x, y, radius, color) {
@@ -114,7 +119,7 @@ let score = 0
 let playerHealth = 100
 
 function animate() {
-    requestAnimationFrame(animate)
+    const animateAll = requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.x += player.vx // updating player position based on its velocity
     player.y += player.vy
@@ -123,25 +128,40 @@ function animate() {
     // projectile()
     // updateEnemyPos()
     eachEnemy()
-    c.drawImage(img, 300, 300, img.width/50, img.height/50)
+    if (enemies.length === 0) {
+        console.log("hit 0")
+        createEnemies()
+    }
+  
+    // c.drawImage(playerShoot, 100, 100, img.width/50, img.height/50)
+    // playerAnimation()
     
     projectiles.forEach((projectile, idx) => {
     projectile.update()
-        console.log(projectile)
+        
     // Removes the projectiles
     if (projectile.x - projectile.radius < 0 || 
         projectile.x - projectile.radius > canvas.width ||
         projectile.y + projectile.radius < 0 ||
         projectile.y - projectile.radius > canvas.height) {
         setTimeout(() => {
-            projectiles.splice(idx, 1)
+            projectiles.splice(idx, 1) //removes 1 projectile from the array
         }, 0)
         }
     })
-}
 
-function eachProjectiles() {
+    function gameOver() {
+        cancelAnimationFrame(animateAll)
+        const endScreen = document.querySelector('#gameOver')
+        endScreen.style.display
+        gameOverScreen.style.display = 'block' // allows our hidden gameover screen to be displayed once our health hits 0
+        scoreEle.innerHTML = score // allows score to be displayed
+        console.log("Game Over")
+    }
 
+    if (playerHealth === 0) {
+        gameOver()
+    }
 }
 
 
@@ -152,45 +172,33 @@ function eachEnemy() {
 
         projectiles.forEach((projectile, pidx) => {
             const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y) // measures the distance from projectile to the enemy
-            console.log(distance)
 
         if (distance - enemy.radius - projectile.radius < 1) { // we need to check the enemy radius and the projectile radius
             enemies.splice(idx, 1) // removing a single enemy at the specific index
             projectiles.splice(pidx, 1) // removing a single projectile at the specific index
         
             score += 10 // adds 10 to the score
-            scoreboard.innerHTML = score // shows player score on the screen
+            scoreboard.innerHTML = `Score: ${score}` // shows player score on the screen
         }
         })
 
             const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-        if (distance - enemy.radius - (player.radius) < 1 ) {
-            playerHealth -= 0.2
-            health.innerHTML = playerHealth
+        if (distance - enemy.radius - (player.radius) < 1 ) { //distance between player and enemy
+            playerHealth -= 5 // subtracts 0.2 to playerhealth for every millisecond of contact
+            health.innerHTML = `Health: ${playerHealth}` // displays changing health
         }
     })
 }
 
 
-// function projectile() {
-//     let xdir = bullet.x 
-//     let ydir = bullet.y
-
-//     if (xdir > 0) {
-//         xdir += 3
-//     } else {
-//         xdir -= 3
-//     }
-
-//     if (ydir > 0) {
-//         ydir += 3
-//     } else {
-//         ydir -= 3
-//     }
-// }
-
 function createEnemies() {
-    for (let i = 0; i < 3; i++) {
+    let maxEnemy = 3
+    
+    // if (enemies.length === 0) {
+    //     maxEnemy += 3
+    //  }
+
+    for (let i = 0; i < maxEnemy; i++) {
     const randomColor = ['red', 'blue', 'green', 'black']
     const randomValue = randomColor[Math.floor(randomColor.length * Math.random())];
 
@@ -201,9 +209,10 @@ function createEnemies() {
         y: 1
     }
 
-    enemies.push(new Enemy(x, y, 20, randomValue, velocity))
+    enemies.push(new Enemy(x, y, 20, randomValue, velocity)) // pushing newly created enemy into array
     }
 }
+
 
 addEventListener('click', (e) => {
     const angle = Math.atan2(e.clientY - (player.y + player.radius), e.clientX - (player.x + player.radius))   // - 75 for y    - 30 for x seems to be the best for accuracy
@@ -215,7 +224,31 @@ addEventListener('click', (e) => {
     projectiles.push(new Projectile(player.x - player.radius, player.y - player.radius, 5, 'red', velocity))
 })
 
+// SPRITE ANIMATION 
 
+// let columnIdx = 5
+// let rowIdx = 5
+
+// let frameWidth = playerShoot.width / columnIdx
+// let frameHeight = playerShoot.height / rowIdx
+
+// let currentFrame = 0
+
+// setInterval(function() {
+//     currentFrame++ 
+
+//     let maxFrame = columnIdx * rowIdx - 1
+//     if (currentFrame > maxFrame) {
+//         currentFrame = 0
+//     }
+
+//     let column = currentFrame % columnIdx
+//     let row = currentFrame % rowIdx
+    
+//     c.clearRect(0, 0, canvas.width, canvas.height)
+//     c.drawImage(playerShoot, column * frameWidth, row * frameHeight, frameWidth, frameHeight,
+//                 10, 30, frameWidth, frameHeight)
+// }, 100)
 
 
 animate();
